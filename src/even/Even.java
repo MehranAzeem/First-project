e198460548a89d151be04e19e607e7bbc28dac69
MehranAzeem <153623232+MehranAzeem@users.noreply.github.com>
2023-12-12 13:49:32 -0800

    Add files via upload

---
+++ src/even/Even.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package even;
+import java.util.Scanner;
+
+        
+/**
+ *
+ * @author ic
+ */
+public class Even {
+
+    
+    
+    
+               
+    
+       
+    public static void main(String[] args) {
+        
+        
+        Scanner sc=new Scanner(System.in);
+        
+        
+        System.out.println("enter any numbers"); 
+        
+        
+        int odd =sc.nextInt();
+        
+        
+        
+            if (odd%2==0){
+                System.out.println(odd+"this is even no");
+                
+            }
+            
+            else {
+                System.out.println("this is odd no");
+           
+            
+           
+                
+                
+               
+            }
+        
+    
+        //for(int i=0;i<=50;i++)
+         //{
+           // if (i%2==1){
+             //   System.out.println(i);
+               
+                
+            }//
+             
+        
+        // TODO code application logic here
+  //  }
+//    //
+}
