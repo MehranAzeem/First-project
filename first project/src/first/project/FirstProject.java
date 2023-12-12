1b183784db50d8076ea9c2845d4266cd20443273
MehranAzeem <153623232+MehranAzeem@users.noreply.github.com>
2023-12-12 13:47:15 -0800

    Add files via upload

---
+++ first project/src/first/project/FirstProject.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first.project;
+
+/**
+ *
+ * @author ic
+ */
+public class FirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //int age = 16;
+        
+        //if(age >= 18)
+        //{
+            //System.out.println("You Can Drive");
+        //}
+       // else
+       // {
+     //       System.out.println("You Cannot Drive");
+        
+   // }
+    
+//}
+      String worker = ali;
+    
+      
+     if(worker.equals("mehran") )
+             
+     {
+    System.out.println("yes");
+    
+}
+else 
+{
+System.out.println("no");
+
+}
+    
